6c06f5445c6f99cc7a68c9bb3a924fa3c147a6d4
iamgreenintro <iamgreenintro@outlook.com>
2024-07-12 00:04:56 +0200

    add unique constraint for genre name

--- sources/BlockBusters.Database/dbo/Tables/genres.sql
+++ sources/BlockBusters.Database/dbo/Tables/genres.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[genres] (
     [id]    INT           IDENTITY (1, 1) NOT NULL,
     [genre] NVARCHAR (50) NOT NULL,
+    CONSTRAINT [UQ_GenreName] UNIQUE ([genre]),
     CONSTRAINT [PK_genres] PRIMARY KEY CLUSTERED ([id] ASC)
 );
 
